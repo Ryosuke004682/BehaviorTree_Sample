0def1b70a8c1e455072c21596ed1670fb7878f29
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-27 15:10:27 +0900

    sequenceのコート改良、Selectorの追加

--- BehaviorTree_Sample/Assets/Behavior_Script/Selector.cs
+++ BehaviorTree_Sample/Assets/Behavior_Script/Selector.cs
@@ -1,33 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    
 
 namespace BehaviorTree
 {
     /// <summary>
     /// ノードクラスから派生させる。
     /// </summary>
-    public class Sequence : Node
+    public class Selector : Node
     {
+        public Selector() : base() { }
+        public Selector(List<Node> children) : base(children) { }
+
         public override NodeState Evaluate()
         {
-            bool anyChildIsRunning = false;
-
-            foreach(Node node in children)
+            foreach (Node node in children)
             {
-                switch(node.Evaluate())
+                switch (node.Evaluate())
                 {
                     case NodeState.FAILUED:
-                        state = NodeState.FAILUED;
                         return state;
 
 
                     case NodeState.SUCCESS:
+                        state = NodeState.SUCCESS;
                         continue;
 
                     case NodeState.RUNNING:
-                        anyChildIsRunning = true;
+                        state = NodeState.RUNNING;
                         continue;
 
                     default:
@@ -36,7 +36,7 @@ namespace BehaviorTree
                 }
             }
 
-            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            state = NodeState.FAILUED;
             return state;
 
         }
